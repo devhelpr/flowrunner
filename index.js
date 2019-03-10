4c2940a17c079f2a8e6faac51375746750f504b1
Maikel van de Lisdonk <maikel@devhelpr.com>
2019-03-10 11:03:51 +0100

    Export classes directly without function

--- index.js
+++ index.js
@@ -5,13 +5,9 @@ let flowTaskPackageType = require("./FlowTaskPackageType.js");
 let humanFlowToMachineFlow = require('./HumanFlowToMachineFlow.js');
 let flowTask = require('./FlowTask.js');
 
-function createFlowRunner() {
-	return {
-		flowEventRunner: flowEventRunner,
-		flowTaskPackageType: flowTaskPackageType,
-		humanFlowToMachineFlow: humanFlowToMachineFlow,
-		flowTask: flowTask
-	}
-}
-
-module.exports = createFlowRunner;
+module.exports = {
+	FlowEventRunner: flowEventRunner,
+	FlowTaskPackageType: flowTaskPackageType,
+	HumanFlowToMachineFlow: humanFlowToMachineFlow,
+	FlowTask: flowTask
+};
