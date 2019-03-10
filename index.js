a00603e7ec097b86a1804cbc4b86c276a0114f26
Maikel van de Lisdonk <maikel@devhelpr.com>
2019-03-10 09:17:05 +0100

    flowtask

--- index.js
+++ index.js
@@ -3,12 +3,14 @@
 let flowEventRunner = require("./FlowEventRunner");
 let flowTaskPackageType = require("./FlowTaskPackageType.js");
 let humanFlowToMachineFlow = require('./HumanFlowToMachineFlow');
+let flowTask = require('./flowTask');
 
 function createFlowRunner() {
 	return {
 		flowEventRunner: flowEventRunner,
 		flowTaskPackageType: flowTaskPackageType,
-		humanFlowToMachineFlow: humanFlowToMachineFlow
+		humanFlowToMachineFlow: humanFlowToMachineFlow,
+		flowTask: flowTask
 	}
 }
 
